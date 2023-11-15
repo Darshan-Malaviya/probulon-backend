f4362c4c229c995d26eba89a9932804ce8c10f9c
Darshan Malaviya <darshanmalaviya163@gmail.com>
2023-11-15 15:36:14 +0530

    RabbitMQ configuration and connection checked

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-var express = require('express');
+var express = require("express");
+const amqp = require("amqplib");
 var app = express();
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
+const queue = "test_queue";
+const text = {
+    item_id: "macbook",
+    text: "This is a sample message to send receiver to check the ordered Item Availablility",
+  };
+
+async function sendData() {
+    let connection;
+    try {
+      connection = await amqp.connect("amqp://user:user123@79.143.90.196:5672");
+      const channel = await connection.createChannel();
+  
+      await channel.assertQueue(queue, { durable: false });
+      channel.sendToQueue(queue, Buffer.from(JSON.stringify(text)));
+      console.log(" [x] Sent '%s'", text);
+      await channel.close();
+    } catch (err) {
+      console.warn(err);
+    } finally {
+      if (connection) await connection.close();
+    }
+}
+
+app.get("/send-msg", async (req, res) => {
+    sendData();
+    res.send("Message Sent"); //response to the API request
+    
+})
+
+app.get("/", function (req, res) {
+    res.send("Hello world!");   
 });
 
 app.listen(3000);
