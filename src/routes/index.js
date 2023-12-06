f6dd8bb746f72620998cf3e5bdab58ca94c4e442
Darshan Malaviya <darshanmalaviya163@gmail.com>
2023-12-06 17:14:21 +0530

    /api path changed

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const userRoutes = require('./user')
 const deviceRoutes = require('./device')
 const authRoutes = require('./auth')
 module.exports = (app) => {
-  app.get("/", (req, res) => {
+  app.get("/api", (req, res) => {
     res.json({
       message: "These are Probulon APIs",
       api_health: "good",
