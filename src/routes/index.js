fe0ce289c59dd47e78d901ffa65e4b0d77e7015d
Darshan Malaviya <darshanmalaviya163@gmail.com>
2023-12-06 19:01:51 +0530

    routes changed

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,8 +9,15 @@ module.exports = (app) => {
       api_version: "V1.0.0",
     });
   });
+  app.get("/hello", (req, res) => {
+    res.json({
+      message: "These are Probulon APIs",
+      api_health: "good",
+      api_version: "V1.0.0",
+    });
+  });
 
-  app.use("/api/v1/users", userRoutes)
-  app.use("/api/v1/devices", deviceRoutes)
-  app.use("/api/v1/auth", authRoutes)
+  app.use("/v1/users", userRoutes)
+  app.use("/v1/devices", deviceRoutes)
+  app.use("/v1/auth", authRoutes)
 };
