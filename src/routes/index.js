633735f3fbafeb2d688d7c321afe91c47041a356
Darshan Malaviya <darshanmalaviya163@gmail.com>
2023-12-06 17:22:53 +0530

    removed api path

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const userRoutes = require('./user')
 const deviceRoutes = require('./device')
 const authRoutes = require('./auth')
 module.exports = (app) => {
-  app.get("/api", (req, res) => {
+  app.get("/", (req, res) => {
     res.json({
       message: "These are Probulon APIs",
       api_health: "good",
