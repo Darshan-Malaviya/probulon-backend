0bcb37ccbee854480ee1996029476fc8aebda40c
Harmish Babariya <harmishbabariya@gmail.com>
2023-12-16 12:45:16 +0530

    added morgan logger

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+const morgan = require('morgan')
+
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms'))
 require("dotenv").config();
 require('./config/config')(app)
 require('./config/dbConfig')()
