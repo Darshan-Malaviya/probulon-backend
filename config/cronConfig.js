3168334a957e75969e9e563c22a8fdd4f781bdd0
Harmish Babariya <harmishbabariya@gmail.com>
2023-12-17 16:43:10 +0530

    feat: node-cron added

--- config/cronConfig.js
+++ config/cronConfig.js
@@ -1,21 +1,24 @@
-const { CronJob } = require('cron');
+const cron = require('node-cron');
 const { preparePublisher } = require('../src/services/mqtt/publisher');
 
-
 function createCronJob(schedule, timeZone) {
-  return new CronJob(schedule, () => preparePublisher('test', { 
-    "message": {
-      "isLocked": false
-    }
-  }), null, true, timeZone);
+    return cron.schedule(schedule, () => {
+        preparePublisher('test', {
+            "message": {
+                "isLocked": false
+            }
+        });
+    }, {
+        timezone: timeZone
+    });
 }
 
 module.exports = function (app) {
-app.post('/schedule/scenarios', (req, res) => {
-  const { schedule, timeZone } = req.body;
-  const newJob = createCronJob(schedule, timeZone);
-  newJob.start();
+    app.post('/schedule/scenarios', (req, res) => {
+        const { schedule, timeZone } = req.body;
+        const newJob = createCronJob(schedule, timeZone);
+        newJob.start();
 
-  res.status(200).json({ message: 'New scenario scheduled.' });
-});
-}
+        res.status(200).json({ message: 'New scenario scheduled.' });
+    });
+};
