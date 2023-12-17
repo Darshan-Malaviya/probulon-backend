bd78763a60e74a647a236c6697baf28898190d6a
Harmish Babariya <harmishbabariya@gmail.com>
2023-12-17 17:08:09 +0530

    added api to stop cron job

--- config/cronConfig.js
+++ config/cronConfig.js
@@ -1,8 +1,11 @@
 const cron = require('node-cron');
 const { preparePublisher } = require('../src/services/mqtt/publisher');
 
+const scheduledJobs = {}; 
+
 function createCronJob(schedule, timeZone) {
-    return cron.schedule(schedule, () => {
+    const jobId = `job_${Date.now()}`; 
+    scheduledJobs[jobId] = cron.schedule(schedule, () => {
         preparePublisher('test', {
             "message": {
                 "isLocked": false
@@ -11,14 +14,33 @@ function createCronJob(schedule, timeZone) {
     }, {
         timezone: timeZone
     });
+
+    return jobId; 
+}
+
+function stopCronJob(jobId) {
+    if (scheduledJobs[jobId]) {
+        scheduledJobs[jobId].stop(); 
+        delete scheduledJobs[jobId]; 
+        return true; 
+    }
+    return false; 
 }
 
 module.exports = function (app) {
     app.post('/schedule/scenarios', (req, res) => {
         const { schedule, timeZone } = req.body;
-        const newJob = createCronJob(schedule, timeZone);
-        newJob.start();
+        const jobId = createCronJob(schedule, timeZone);
+        res.status(200).json({ message: 'New scenario scheduled.', jobId });
+    });
 
-        res.status(200).json({ message: 'New scenario scheduled.' });
+    app.post('/stop/scenario', (req, res) => {
+        const { jobId } = req.body;
+        const stopped = stopCronJob(jobId);
+        if (stopped) {
+            res.status(200).json({ message: 'Scenario stopped.' });
+        } else {
+            res.status(404).json({ error: 'Scenario not found.' });
+        }
     });
 };
